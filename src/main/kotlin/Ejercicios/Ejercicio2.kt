0c69f9fdda48cca110639b4ae71c2a0de7994ff8
Katanac <tatisred@hotmail.com>
2022-06-06 20:47:10 -0500

    ejercicios clase

--- src/main/kotlin/Ejercicios/Ejercicio2.kt
+++ src/main/kotlin/Ejercicios/Ejercicio2.kt
@@ -1,4 +1,18 @@
 package Ejercicios
 
-class Ejercicio2 {
+import kotlin.math.ceil
+
+fun main() {
+    print("Estudiantes Gordos:")
+    val estudiantesGordos = readLine()!!.toInt()
+    print("Estudiantes Flacos:")
+    val estudiantesFlacos = readLine()!!.toInt()
+    print("Sillas:")
+    val sillas = readLine()!!.toInt()
+    print("los buses son:${bus(estudiantesGordos, estudiantesFlacos, sillas)}")
+}
+
+fun bus(gordos: Int, flacos: Int, sillas: Int): Int {
+    require(sillas > 0 && gordos >= 0 && flacos >= 0)
+    return ceil((gordos * 2 + flacos / sillas).toDouble()).toInt()
 }
