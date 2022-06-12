52e0b5369bd6c7a6c39addc05245ef74ce189dd8
Yannis <yannis.s.avila.p>
2022-06-12 11:13:47 -0500

    Solucion ejericicio 1y6

--- src/main/kotlin/Ejercicios/Ejercicio1.kt
+++ src/main/kotlin/Ejercicios/Ejercicio1.kt
@@ -1,4 +1,20 @@
 package Ejercicios
 
 class Ejercicio1 {
+
+}
+
+fun main(){
+    //Entradas
+        println("Ingrese x: ")
+        val x= readLine()!!.toDouble()
+        println("Ingrese y: ")
+        val y= readLine()!!.toDouble()
+
+    // Salidas
+        println("El resultado es: ${ejercicio_uno(x,y)}")
+}
+
+fun ejercicio_uno(x:Double, y:Double):Double{
+    return Math.pow(x,2.0)-(2*x*y)+Math.pow(y,2.0)
 }
