e2edb2b243e135a837a0869c0b26dc19a3b9ba0e
Yannis <yannis.s.avila.p>
2022-06-12 21:25:42 -0500

    Cargar todos los nuevos cambios

--- src/main/kotlin/Ejercicios/Ejercicio14.kt
+++ src/main/kotlin/Ejercicios/Ejercicio14.kt
@@ -1,4 +1,39 @@
 package Ejercicios
 
-class Ejercicio14 {
+// 4. Escriba una función, y el correspondiente programa en Kotlin en el que a
+//partir de los nombres y las edades de tres hermanos, obtenga el nombre
+//del hermano mayor
+
+
+
+
+class Persona {
+
+    var nombre = readLine()!!.toString()
+    var edad= readLine()!!.toInt()
+   //
+
+    constructor(nombre: String, edad: Int) {
+        println("Ingrese nombre primer hermano")
+        println("Ingrese edad primer hermano")
+        this.nombre = nombre
+        this.edad = edad
+    }
 }
+
+fun calculoPersonaMayor(p1: Persona, p2: Persona, p3: Persona): Pair<String, Int> {
+
+    val listadoPersonas: List<Persona> = listOf(p1, p2, p3)
+
+    val mayorEntreP1P2 = Math.max(p1.edad, p2.edad)
+    val mayor = Math.max(mayorEntreP1P2, p3.edad)
+
+    listadoPersonas.forEach {
+        if (it.edad == mayor) {
+            return Pair(it.nombre, it.edad)
+        }
+    }
+    return Pair("persona no existe", 0)
+}
+
+
--- src/main/kotlin/Ejercicios/Ejercicio4.kt
+++ src/main/kotlin/Ejercicios/Ejercicio4.kt
@@ -1,4 +1,27 @@
 package Ejercicios
 
-class Ejercicio4 {
+fun main(){
+    //entradas
+    println("Ingrese el sueldo recibido de Pedro ")
+    val s= readLine()!!.toDouble()
+    val sga=sueldo_gastado_arriendo(s)
+    val sgc=sueldo_gastado_comida(s)
+    //Salidas
+    println("Gasto en arriendo: $sga ")
+    println("Gasto en comida: $sgc")
+   println("Dinero final de Pedro: ${dinero_final(s,sga,sgc)}  ")
 }
+
+fun sueldo_gastado_arriendo(s:Double):Double{
+    val sga= s*0.40
+    return sga
+}
+fun sueldo_gastado_comida(s:Double):Double{
+    val sgc= s*0.15
+    return sgc
+}
+fun dinero_final(s:Double,sga:Double,sgc:Double):Double{
+    return s-(sga+sgc)
+}
+
+
